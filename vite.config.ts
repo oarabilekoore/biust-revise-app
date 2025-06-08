a1da2ab1896519076eff863dfe944ae66ba5390a
Oarabile Koore <oarabilekoore@protonmail.com>
2025-06-08 22:36:23 +0200

    ...

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: "/buist-revise-app/",
+    base: "/biust-revise-app/",
     build: {
         outDir: "dist",
         sourcemap: true,
